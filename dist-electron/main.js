7d409500cebc977fa55703ed2475bf13ce831fdf
Arif Zeeshan <26674272+arifzeeshan@users.noreply.github.com>
2024-04-21 00:35:45 +0530

    Updated application icon and improved code formatting
    The application icon has been updated to a new one. The codebase has also undergone some formatting improvements for better readability. Additionally, the Greeting component now uses the Card UI component for its layout.

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -10,7 +10,8 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win;
 function createWindow() {
   win = new electron.BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    // icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(__dirname, "..", "public", "icons", "hello_world.icns"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js")
     }
--- src/components/pages/greeting.tsx
+++ src/components/pages/greeting.tsx
@@ -1,14 +1,16 @@
+import { Card } from "../ui/card";
+
 interface GreetingProps {
   name: string;
 }
 
 function Greeting({ name }: GreetingProps) {
   return (
-    <>
+    <Card className="p-6">
       <div className="flex gap-3">
         <h1 className="font-medium">Hello, {name}!</h1>
       </div>
-    </>
+    </Card>
   );
 }
 
